cf9f1eb8166571a4a20a0a9543891c9a1ba3fff6
George <Ignit218@yandex.ru>
2023-04-18 21:12:25 +0300

    Struct changes

--- functions.h
+++ functions.h
@@ -1,15 +1,18 @@
 #pragma once
+
 struct node_t
 {
         struct node_t* next;
         struct node_t* prev;
         int data;
+	int value;
 };
 
 struct list_t
 {
         struct node_t* head;
-        struct node_t* tail;
+	struct node_t* fst_dist;
+	int size;
 };
 
 struct node_t* append(struct node_t* bottom, int a);
