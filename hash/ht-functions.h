26cf3d06c740ab89fe98f1c5164c1476ecfa6a48
Minimullina Aliya <minimullina.ae@phystech.edu>
2023-05-20 13:03:40 +0300

    trying to make it compile

--- hash/ht-functions.h
+++ hash/ht-functions.h
@@ -22,6 +22,9 @@ void solve_collision(long index, HtElem * elem, HashTable * table);
 
 void ht_insert(HashTable * table, long data, Node_t * value);
 
+
+Node_t* ht_search(HashTable* table, long data);
+
 void ht_delete(HashTable * table, long data);
 
 Node_t *is_element_in_ht(HashTable * table, long data);
