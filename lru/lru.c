28c039573fb417da385ccac20cd33afa9357fc94
George <Ignit218@yandex.ru>
2023-05-20 15:50:03 +0300

    Const used and K&R formating

--- lru/lru.c
+++ lru/lru.c
@@ -7,92 +7,87 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "lru.h"
-
 Queue *createQueue(long m)
 {
-    Queue *queue = (Queue *) calloc(1, sizeof(Queue));
-    queue->max_cell = m;
-    queue->full_cell = 0;
-    queue->top = queue->end = NULL;
-    return queue;
+	Queue *queue = (Queue *) calloc(1, sizeof(Queue));
+	queue->max_cell = m;
+	queue->full_cell = 0;
+	queue->top = queue->end = NULL;
+	return queue;
 }
 
 int isQueueEmpty(Queue * queue)
 {
-    return queue->end == NULL;
+	return queue->end == NULL;
 }
 
 void add_qnode(Queue * queue, long page, QNode ** hash)
 {
-    QNode *tmp = (QNode *) calloc(1, sizeof(QNode));
-    assert(queue->full_cell <= queue->max_cell);
-    tmp->page = page;
-    tmp->prev = NULL;
-    tmp->next = queue->top;
-    if (queue->top != NULL)
-        queue->top->prev = tmp;
-    else {
-        queue->end = tmp;
-    }
-    queue->top = tmp;
-    queue->full_cell += 1;
-    hash[page] = tmp;
-}
+	QNode *tmp = (QNode *) calloc(1, sizeof(QNode));
+	assert(queue->full_cell <= queue->max_cell);
+	tmp->page = page;
+	tmp->prev = NULL;
+	tmp->next = queue->top;
+	if (queue->top != NULL)
+		queue->top->prev = tmp;
 
+	else {
+		queue->end = tmp;
+	}
+	queue->top = tmp;
+	queue->full_cell += 1;
+	hash[page] = tmp;
+}
 
 int isQueueFull(Queue * queue)
 {
-    return queue->max_cell == queue->full_cell;
+	return queue->max_cell == queue->full_cell;
 }
 
 int del_qnode(Queue * queue, QNode ** hash)
 {
-    QNode *tmp = queue->end;
-    hash[queue->end->page] = NULL;
-    if (queue->full_cell == 1) {
-        QNode *tmp = queue->end;
-        queue->top = queue->end = NULL;
-    } else {
-        queue->end = queue->end->prev;
-        queue->end->next = NULL;
-    }
-   
-    queue->full_cell -= 1;
-    free(tmp);
-    return 0;
+	QNode *tmp = queue->end;
+	hash[queue->end->page] = NULL;
+	if (queue->full_cell == 1) {
+		QNode *tmp = queue->end;
+		queue->top = queue->end = NULL;
+	} else {
+		queue->end = queue->end->prev;
+		queue->end->next = NULL;
+	}
+	queue->full_cell -= 1;
+	free(tmp);
+	return 0;
 }
 
 int lru(long page, Queue * queue, QNode ** hash)
 {
-    QNode *tmp = hash[page];
-    if (isQueueEmpty(queue)) {
-        add_qnode(queue, page, hash);
-        return 0;
-    }
+	QNode *tmp = hash[page];
+	if (isQueueEmpty(queue)) {
+		add_qnode(queue, page, hash);
+		return 0;
+	}
+	if (tmp == NULL) {
+		if (isQueueFull(queue)) {
+			del_qnode(queue, hash);
+		}
+		add_qnode(queue, page, hash);
+		return 0;
+	} else {
+		if ((queue->full_cell == 1) || (tmp == queue->top)) {
+			return 1;
+		}
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
 
-    if (tmp == NULL) {
-        if (isQueueFull(queue)) {
-            del_qnode(queue, hash);
-        }
-        add_qnode(queue, page, hash);
-        return 0;
-    } else {
-        if ((queue->full_cell == 1) || (tmp == queue->top)) {
-            return 1;
-        }
-        tmp->prev->next = tmp->next;
-        if (tmp->next != NULL)
-            tmp->next->prev = tmp->prev;
-        else
-            queue->end = tmp->prev;
-
-        assert(tmp == hash[page]);  
-        hash[page] = NULL;
-        free(tmp);
-        queue->full_cell -= 1;
-
-        add_qnode(queue, page, hash);
-        return 1;
-    }
+		else
+			queue->end = tmp->prev;
+		assert(tmp == hash[page]);
+		hash[page] = NULL;
+		free(tmp);
+		queue->full_cell -= 1;
+		add_qnode(queue, page, hash);
+		return 1;
+	}
 }
-
--- main.c
+++ main.c
@@ -10,95 +10,102 @@
 
 int main()
 {
-    //FIXME:temporary code 
-	Node_t** hash_RRIP;
-    QNode** hash_LRU;
-    long hashsize; 
-    //
+	//FIXME:temporary code 
+	Node_t **hash_RRIP;
+	QNode **hash_LRU;
+	long hashsize;
+	//
 
-    long cache_size, num_req, page, count_RRIP, count_LRU; 
-    /* 
-        num_req is an overall number of requests
-        page is a current request
-        count_RRIP tracks a number of cache hits in RRIP replacement
-        count_LRU tracks a number of cache hits in LRU replacement
-    */
-    List_t* list;
-    List_t* list2;
-    Queue* queue;
-    HashTable *table = create_table();
+	long cache_size, num_req, page, count_RRIP, count_LRU;
+	/* 
+	   num_req is an overall number of requests
+	   page is a current request
+	   count_RRIP tracks a number of cache hits in RRIP replacement
+	   count_LRU tracks a number of cache hits in LRU replacement
+	 */
+	List_t *list;
+	List_t *list2;
+	Queue *queue;
+	HashTable *table = create_table();
 
-    //scanning
-    while(!scanf("%ld", &cache_size) || (cache_size <= 0)) {
-        printf("Waiting for the cache size... It must be positive number\n");
-    } 
+	//scanning
+	while (!scanf("%ld", &cache_size) || (cache_size <= 0)) {
+		printf
+			("Waiting for the cache size... It must be positive number\n");
+	}
 
-    while(!scanf("%ld", &num_req) || (num_req <= 0)) {
-        printf("Waiting for the overall number of requests... It must be positive number\n");
-    }
-    
+	while (!scanf("%ld", &num_req) || (num_req <= 0)) {
+		printf
+			("Waiting for the overall number of requests... It must be positive number\n");
+	}
 
-    //FIXME: temporary code
-    hash_RRIP = calloc(num_req + 1, sizeof(Node_t *));
-    hash_LRU = calloc(num_req + 1, sizeof(QNode *));
-   
-    if ((hash_RRIP == NULL) || (hash_LRU == NULL)) {
-        fprintf(stderr, "Memory exhausted\n");
-        abort();
-    }   
-    hashsize = num_req + 1;
-    //
 
-    list = create_list(cache_size);
-    list2 = create_list(cache_size);
-    queue = createQueue(cache_size);
-    count_LRU = 0;
-    count_RRIP = 0;
+	//FIXME: temporary code
+	hash_RRIP = calloc(num_req + 1, sizeof(Node_t *));
+	hash_LRU = calloc(num_req + 1, sizeof(QNode *));
 
-    //work begins...
-    for (long i = 0; i < num_req; i++) 
-    {
-        while(!scanf("%ld", &page) || (page <= 0)) {
-            printf("Waiting for your request... It must be positive number\n");
-        }   
-        
-        //FIXME: temporarily we need to update hash each time
-        if (hashsize - 1 < page) {
-            hash_RRIP = (Node_t **) realloc(hash_RRIP, (page + 1) * sizeof(Node_t *));
-            for (long i = hashsize; i <= page; i++)
-                hash_RRIP[i] = NULL;
+	if ((hash_RRIP == NULL) || (hash_LRU == NULL)) {
+		fprintf(stderr, "Memory exhausted\n");
+		abort();
+	}
+	hashsize = num_req + 1;
+	//
 
-            hash_LRU = (QNode **) realloc(hash_LRU, (page + 1) * sizeof(QNode* ));
-            for (long i = hashsize; i <= page; i++)
-                hash_LRU[i] = NULL;
+	list = create_list(cache_size);
+	list2 = create_list(cache_size);
+	queue = createQueue(cache_size);
+	count_LRU = 0;
+	count_RRIP = 0;
 
-            if ((hash_RRIP == NULL) || (hash_LRU == NULL)) {
-                fprintf(stderr, "Memory exhausted(during realloc)\n");
-                abort();
-            }   
+	//work begins...
+	for (long i = 0; i < num_req; i++) {
+		while (!scanf("%ld", &page) || (page <= 0)) {
+			printf
+				("Waiting for your request... It must be positive number\n");
+		}
 
-            hashsize = page + 1;
-        }
-        //
-        
+		//FIXME: temporarily we need to update hash each time
+		if (hashsize - 1 < page) {
+			hash_RRIP =
+				(Node_t **) realloc(hash_RRIP,
+						(page + 1) * sizeof(Node_t *));
+			for (long i = hashsize; i <= page; i++)
+				hash_RRIP[i] = NULL;
+
+			hash_LRU =
+				(QNode **) realloc(hash_LRU, (page + 1) * sizeof(QNode *));
+			for (long i = hashsize; i <= page; i++)
+				hash_LRU[i] = NULL;
+
+			if ((hash_RRIP == NULL) || (hash_LRU == NULL)) {
+				fprintf(stderr, "Memory exhausted(during realloc)\n");
+				abort();
+			}
+
+			hashsize = page + 1;
+		}
+		//
+
 		//count_LRU += lru(page, queue, hash_LRU);
-        count_LRU += replacement_RRIP1(page, list2, hash_RRIP);
-        count_RRIP += replacement_RRIP(page, list, table);
-    }
+		count_LRU += replacement_RRIP1(page, list2, hash_RRIP);
+		count_RRIP += replacement_RRIP(page, list, table);
+	}
 
-    assert((count_LRU >= 0) && (count_RRIP >= 0) && "Something went wrong, code doesn't work correctly");  
-    printf("Number of cache hits:\nfor RRIP %ld\nfor LRU %ld\n", count_RRIP, count_LRU);
+	assert((count_LRU >= 0) && (count_RRIP >= 0)
+			&& "Something went wrong, code doesn't work correctly");
+	printf("Number of cache hits:\nfor RRIP %ld\nfor LRU %ld\n",
+			count_RRIP, count_LRU);
 
-    if ((count_LRU > count_RRIP) && (count_LRU != 0))
-        printf("Suprisingly LRU has performed better...\n");
+	if ((count_LRU > count_RRIP) && (count_LRU != 0))
+		printf("Suprisingly LRU has performed better...\n");
 
-    if ((count_LRU == 0) && (count_RRIP == 0))
-        printf("OMG, no cache hits at all...");
-    
+	if ((count_LRU == 0) && (count_RRIP == 0))
+		printf("OMG, no cache hits at all...");
+
 
 	delete_list(list);
-    //delete_queue(queue);
+	//delete_queue(queue);
 
 	//free(hash) is needed
-    return 0;
+	return 0;
 }
