09deaa095ce53e3d1c623521d79119642ce5ac67
Minimullina Aliya <minimullina.ae@phystech.edu>
2023-05-21 14:39:58 +0300

    Updated makefile

--- Makefile
+++ Makefile
@@ -1,24 +1,36 @@
+CC=gcc
+CFLAGS= 
+EXECUTABLE=program
+
 program: main.o functions.o lru.o ht-linked-list.o ht-functions.o 
-	gcc -g -O1 -o program main.o functions.o lru.o ht-linked-list.o ht-functions.o 
+	@$(CC) -g $(CFLAGS) -o program main.o functions.o lru.o ht-linked-list.o ht-functions.o 
 
 main.o: main.c ./lru/lru.h functions.h ./hash/ht-functions.h
-	gcc -c main.c
+	@$(CC) $(CFLAGS) -g -c main.c
 
 functions.o: functions.c ./lru/lru.h functions.h ./hash/ht-linked-list.h ./hash/ht-functions.h
-	gcc -c functions.c
+	@$(CC) $(CFLAGS) -g -c functions.c
 
 lru.o: ./lru/lru.c ./lru/lru.h
-	gcc -c ./lru/lru.c
+	@$(CC) $(CFLAGS) -g -c ./lru/lru.c
 
 ht-linked-list.o: ./hash/ht-linked-list.c ./hash/ht-linked-list.h
-	gcc -c ./hash/ht-linked-list.c
+	@$(CC) $(CFLAGS) -g -c ./hash/ht-linked-list.c
 
 ht-functions.o: ./hash/ht-functions.c ./hash/ht-functions.h
-	gcc -c ./hash/ht-functions.c
+	@$(CC) $(CFLAGS) -g -c ./hash/ht-functions.c
 
 test:
+	bash ./tests/bash.sh
 
 clean: 
 	rm *.o program
 	rm *.c~ program
-	rm ./lru/*.c~ program
+	
+
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -12,30 +12,32 @@
 
 int main()
 {
-	Node_t** hash_RRIP;
-    QNode** hash_LRU;
-    long hashsize; 
+    Node_t **hash_RRIP;
+    QNode **hash_LRU;
+    long hashsize;
     HashTable *table;
-    
-    long cache_size, num_req, page, count_RRIP, count_LRU, count_check; 
+
+    long cache_size, num_req, page, count_RRIP, count_LRU, count_check;
     /* 
-        num_req is an overall number of requests
-        page is a current request
-        count_RRIP tracks a number of cache hits in RRIP replacement
-        count_LRU tracks a number of cache hits in LRU replacement
-        count_check tracks a number of cache hits in RRIP using simple hash(just array)
-    */
-    List_t* list;
-    List_t* list_check;
-    Queue* queue;
+       num_req is an overall number of requests
+       page is a current request
+       count_RRIP tracks a number of cache hits in RRIP replacement
+       count_LRU tracks a number of cache hits in LRU replacement
+       count_check tracks a number of cache hits in RRIP using simple hash(just array)
+     */
+    List_t *list;
+    List_t *list_check;
+    Queue *queue;
 
     //scanning
-    while(!scanf("%ld", &cache_size) || (cache_size <= 0)) 
-        printf("Waiting for the cache size... It must be positive number\n");
+    while (!scanf("%ld", &cache_size) || (cache_size <= 0))
+        printf
+            ("Waiting for the cache size... It must be positive number\n");
 
-    while(!scanf("%ld", &num_req) || (num_req <= 0)) 
-        printf("Waiting for the overall number of requests... It must be positive number\n");
-    
+    while (!scanf("%ld", &num_req) || (num_req <= 0))
+        printf
+            ("Waiting for the overall number of requests... It must be positive number\n");
+
     hashsize = num_req + 1;
     hash_RRIP = calloc(hashsize, sizeof(Node_t *));
     hash_LRU = calloc(hashsize, sizeof(QNode *));
@@ -44,48 +46,53 @@ int main()
     list_check = create_list(cache_size);
     queue = createQueue(cache_size);
 
-    if ((hash_RRIP == NULL) || (hash_LRU == NULL) || (table == NULL) || (list == NULL) || (list_check == NULL) || (queue == NULL)) {
+    if ((hash_RRIP == NULL) || (hash_LRU == NULL) || (table == NULL)
+        || (list == NULL) || (list_check == NULL) || (queue == NULL)) {
         fprintf(stderr, "Memory exhausted\n");
         abort();
-    }   
-  
+    }
+
     count_LRU = 0;
     count_RRIP = 0;
     count_check = 0;
 
     //work begins...
-    for (long i = 0; i < num_req; i++) 
-    {
-        while(!scanf("%ld", &page) || (page <= 0)) {
-            printf("Waiting for your request... It must be positive number\n");
-        }   
-   
+    for (long i = 0; i < num_req; i++) {
+        while (!scanf("%ld", &page) || (page <= 0)) {
+            printf
+                ("Waiting for your request... It must be positive number\n");
+        }
+
         if (hashsize - 1 < page) {
             update_hash(&hashsize, page, hash_RRIP, hash_LRU);
         }
-             
-		count_LRU += lru(page, queue, hash_LRU);
+
+        count_LRU += lru(page, queue, hash_LRU);
         count_RRIP += replacement_RRIP(page, list, table);
         count_check += replacement_RRIP1(page, list_check, hash_RRIP);
     }
 
-    assert((count_LRU >= 0) && (count_RRIP >= 0) && (count_check == count_RRIP) && "Something went wrong, code doesn't work correctly"); 
+    assert((count_LRU >= 0) && (count_RRIP >= 0)
+           && (count_check == count_RRIP)
+           && "Something went wrong, code doesn't work correctly");
 
-    printf("Number of cache hits:\nfor RRIP %ld (%ld)\nfor LRU %ld\n", count_RRIP, count_check, count_LRU);
+    printf("Number of cache hits:\nfor RRIP %ld (%ld)\nfor LRU %ld\n",
+           count_RRIP, count_check, count_LRU);
 
-    if ((count_LRU > count_RRIP) && (count_LRU != 0))
+    if ((count_LRU > count_RRIP) && (count_LRU != 0)) {
         printf("Suprisingly LRU has performed better...\n");
+    }
 
-	if ((count_LRU == 0) && (count_RRIP == 0))
-		printf("OMG, no cache hits at all...");
+    if ((count_LRU == 0) && (count_RRIP == 0)) {
+        printf("OMG, no cache hits at all...");
+    }
 
-
-	delete_list(list);
+    delete_list(list);
     delete_list(list_check);
     free_table(table);
     delete_hashLRU(hash_LRU);
     delete_hashRRIP(hash_RRIP);
     delete_queue(queue);
 
-	return 0;
+    return 0;
 }
