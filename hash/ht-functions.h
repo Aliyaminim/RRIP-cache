61c1550a6dfd24a77f2eba7e936e625d5fee68e8
Minimullina Aliya <minimullina.ae@phystech.edu>
2023-05-22 11:36:44 +0300

    Added comments

--- hash/ht-functions.h
+++ hash/ht-functions.h
@@ -1,34 +1,40 @@
 #pragma once
 
-//dimension of hash table, that's prime number
+/* dimension of hash table, that's prime number */
 static const long modular = 1000019;
 
-//simple hash_function, takes long integer data, returns hash of data
+/* simple hash_function, takes long integer data, returns hash of data */
 long hash_function(const long data);
 
-//creates overflow list for hash table, takes a pointer of hash table, returns the pointer to array of hash table linked list nodes
+/* creates overflow list for hash table, takes a pointer of hash table, 
+   returns the pointer to array of hash table linked list nodes */
 NodeHtLl **create_overflow_list(const HashTable * table);
 
-//fully frees the overflow list, takes a pointer of hash table
+/* fully frees the overflow list, takes a pointer of hash table */
 void free_overflow_list(const HashTable * table);
 
-//creates a hash table element, takes long integer dat and node_t value, returns the pointer to an hash table element
+/* creates a hash table element, takes long integer dat and node_t value, 
+   returns the pointer to an hash table element */
 HtElem *create_elem(const long data, Node_t * value);
 
-//creates hash table, returns the pointer of hash table
+/* creates hash table, returns the pointer of hash table */
 HashTable *create_table();
 
-//fully frees the hash table, takes the pointer of hash table
+/* fully frees the hash table, takes the pointer of hash table */
 void free_table(HashTable * table);
 
-//solves a collision of hash table with overflow list, takes long integer data, the pointer to hash table element and the pointer to hash table
+/* solves a collision of hash table with overflow list, takes long integer data, 
+   the pointer to hash table element and the pointer to hash table */
 void solve_collision(const long index, HtElem * elem, HashTable * table);
 
-//inserts an element to the hash table, takes the pointer to hash table, long integer data and node_t (chain) pointer
+/* inserts an element to the hash table, takes the pointer to hash table, long integer data 
+   and node_t (chain) pointer */
 void ht_insert(HashTable * table, const long data, Node_t * value);
 
-//searches an element in the hash table, takes a pointer and long integer data needed to be looked for, returns a pointer of node_t if found, otherwise NULL
+/* searches an element in the hash table, takes a pointer and long integer data needed 
+   to be looked for, returns a pointer of node_t if found, otherwise NULL */
 Node_t *ht_search(HashTable * table, const long data);
 
-//deletes and element from the hash table with reconnecting of the other elements, takes a pointer to the hash table and long integer data needed to be deleted
+/* deletes and element from the hash table with reconnecting of the other elements, 
+   takes a pointer to the hash table and long integer data needed to be deleted */
 void ht_delete(HashTable * table, const long data);
--- hash/ht-linked-list.h
+++ hash/ht-linked-list.h
@@ -1,13 +1,15 @@
 #pragma once
 
-//creates hash table linked list, returns a pointer to the node hash table linked list
+/* creates hash table linked list, returns a pointer to the node hash table linked list */
 NodeHtLl *ht_ll_create();
 
-//inserts an hash table element to hash table linked list, takes the pointers to the NodeHtl list and HtElem element, returns NodeHtLl pointer
+/* inserts an hash table element to hash table linked list, takes the pointers to 
+    the NodeHtl list and HtElem element, returns NodeHtLl pointer*/
 NodeHtLl *ht_ll_insert(NodeHtLl * list, HtElem * elem);
 
-//removes an element from hash table linked list, takes the pointer to NodeHtLl list, returns HtElem pointer
+/* removes an element from hash table linked list, takes the pointer to NodeHtLl list,
+   returns HtElem pointer */
 HtElem *ht_ll_remove(NodeHtLl * list);
 
-//simply frees hash table linked list, takes NodeHtLL list pointer
+/* simply frees hash table linked list, takes NodeHtLL list pointer */
 void ht_ll_free(NodeHtLl * list);
